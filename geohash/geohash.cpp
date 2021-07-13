ee67d0e7b132e23c307f8e6ffc2f5f3bece21097
knowmore <goooog@88.com>
2021-07-13 13:54:00 +0800

    feat: geohash code init

---
+++ geohash/geohash.cpp
@@ -0,0 +1,5 @@
+//
+// Created by km on 7/13/21.
+//
+
+#include "geohash.h"
---
+++ geohash/geohash.h
@@ -0,0 +1,19 @@
+//
+// Created by km on 7/13/21.
+//
+
+#ifndef GEOHASH_GEOHASH_H
+#define GEOHASH_GEOHASH_H
+
+class Point {
+public:
+    double lon;
+    double lat;
+};
+
+class GeoHash {
+    static constexpr
+
+};
+
+#endif //GEOHASH_GEOHASH_H
